50c4ef7cb8315d7b251336d306e5e2f619985cff
Patrick <patrickconnors@college.harvard.edu>
2016-04-05 22:34:50 -0400

    has

--- _build/refs.ml
+++ _build/refs.ml
@@ -13,40 +13,52 @@ type 'a mlist = Nil | Cons of 'a * 'a mlist ref
    a cycle.  You may want a recursive helper function. Don't worry
    about space usage. *)
 let has_cycle (lst : 'a mlist) : bool =
-  let is_traversed (nd1 : 'a) (nd2 : 'a) : bool =
-    if (nd1) == nd2 then true else false
+  let is_traversed (nd1 : 'a) (nd2 : 'a list) : bool =
+    if List.memq nd1 nd2 then true else false
   in
-  let rec check (nd3: 'a) (lst: 'a mlist) : bool =
-    match lst with
-    | Nil -> false
-    | Cons(c,n) ->
-      if is_traversed nd3 c then true
-      else check nd3 !n
-  in
-  let rec iter (lst: 'a mlist) : bool =
+  let rec iter (lst: 'a mlist) (lst_build : 'a list) : bool =
     match lst with
     | Nil -> false
     | Cons(c,n) -> 
-      if (check c !n) then true
-      else iter !n
-  in iter lst
+      if is_traversed c lst_build then true
+      else iter !n (lst_build@[c])
+  in iter lst []
 ;;
 
 
 (* Write a function flatten that flattens a list (removes its cycles
    if it has any) destructively. Again, you may want a recursive
    helper function and you shouldn't worry about space. *)
-(* let flatten (lst : 'a mlist) : unit =
-  if has_cycle lst then
-
-  else () *)
+let flatten (lst : 'a mlist) : unit =
+  let is_traversed (nd1 : 'a) (nd2 : 'a list) : bool =
+    if List.memq nd1 nd2 then true else false
+  in
+  let rec iter (lst: 'a mlist) (lst_build : 'a list) : unit =
+    match lst with
+    | Nil -> ()
+    | Cons(c,n) -> 
+      if is_traversed c lst_build then n:=Nil
+      else iter !n (lst_build@[c])
+  in iter lst []
+;;
 
 (* Write mlength, which nondestructively finds the number of nodes in
    a mutable list that may have cycles. *)
 let mlength (lst : 'a mlist) : int =
-  failwith "mlength not implemented"
+  let is_traversed (nd1 : 'a) (nd2 : 'a list) : bool =
+    if List.memq nd1 nd2 then true else false
+  in
+  let rec len (lst: 'a mlist) (lst_build:'a list) (counter: int) : int =
+    match lst with
+    | Nil -> counter
+    | Cons(c,n) -> 
+      if is_traversed c lst_build then 
+        let counter1 = counter + 1 in counter1
+      else len (!n) (lst_build@[c]) (counter+1)
+  in len lst [] 0
+;;
 
 (* Please give us an honest estimate of how long this part took you to
    complete.  We care about your responses and will use them to help
    guide us in creating future assignments. *)
-let minutes_spent : int = -1
+let minutes_spent : int = 30
